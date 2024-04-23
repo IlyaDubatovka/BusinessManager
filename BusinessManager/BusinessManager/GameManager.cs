5007f0ffa2c4cdaa00d09c45a61b488c14ed8328
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-04-23 13:18:16 +0300

    feat: upgrade GameManager

--- BusinessManager/BusinessManager/GameManager.cs
+++ BusinessManager/BusinessManager/GameManager.cs
@@ -7,8 +7,44 @@ namespace BusinessManager
         public void StartGame()
         {
             InputController inputController = new();
-            Console.WriteLine("Вы предприниматель и вас зовут" );
+            BusinessPool businessPool = new();
+            Console.WriteLine("Вы предприниматель и вас зовут");
             BusinessMan businessMan = new BusinessMan(Console.ReadLine());
+            ShowStartMenu();
+            Console.WriteLine("Игра завершена");
+        }
+
+        public void ShowStartMenu()
+        {
+            Console.WriteLine("1 - Улучшить бизнес");
+            Console.WriteLine("2 - Приобрести новый бизнес");
+            Console.WriteLine("3 - Выйти из игры");
+            Console.Write("Сделайте ваш выбор : ");
+            int input = Console.Read();
+            switch (Convert.ToInt32(((char)input).ToString()))
+            {
+                case 1:
+                    ShowUpgrades();
+                    break;
+                case 2:
+                    ShowFreeBusineses();
+                    break;
+                case 3:
+                    return;
+                default:
+                    Console.WriteLine("Вы сделали неверный выбор");
+                    break;
+            }
+        }
+
+        public void ShowUpgrades()
+        {
+            Console.WriteLine("Улучшили бизнес");
+        }
+
+        public void ShowFreeBusineses()
+        {
+            Console.WriteLine("Купили новый бизнес");
         }
     }
 }
--- BusinessManager/BusinessManager/Program.cs
+++ BusinessManager/BusinessManager/Program.cs
@@ -6,8 +6,8 @@ namespace BusinessManager
     {
         static void Main(string[] args)
         {
-            Business business1 = new Business(1000, "Автомойка",20000);
-            Business business2 = new Business(1200, "Бургерная",12000);
+            GameManager gameManager = new();
+            gameManager.StartGame();
         }
     }
 }
